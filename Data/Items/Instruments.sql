7a35c717ef0bb0753801e5f81eee4e81dfebb1b8
Ahmed <aa87006722@gmail.com>
2024-12-05 15:57:09 +0200

    Add Workbrench Diagram

--- Data/Items/Instruments.sql
+++ Data/Items/Instruments.sql
@@ -11,13 +11,13 @@ insert into Items (Code, Name, Price,  Stock_Level) values ('8499141615449', 'Sc
 insert into Items (Code, Name, Price,  Stock_Level) values ('5814401496275', 'Pulse Oximeter', '5', '2');
 
 -- Instruments I-Type Attrubites
-insert into Instruments (Items_Code ,I_Type) values ('9891957382168', 'Parenteral dosage form administration');
-insert into Instruments (Items_Code ,I_Type) values ('9236706976075', 'Urine Collection');
-insert into Instruments (Items_Code ,I_Type) values ('9160945837954', 'Venous Dosage From Administration');
-insert into Instruments (Items_Code ,I_Type) values ('4001883254479', 'Nebulized-Durg Administration');
-insert into Instruments (Items_Code ,I_Type) values ('5648875210671', 'Body Mass Measurement And Calculation Of Body Water, Lipid, Protein, And Carbohydrate');
-insert into Instruments (Items_Code ,I_Type) values ('4168514353850', 'Measurement Of Blood Pressure');
-insert into Instruments (Items_Code ,I_Type) values ('6965105140966', 'Pregnancy Testing');
-insert into Instruments (Items_Code ,I_Type) values ('9158394740278', 'Blood Glucose Testing');
-insert into Instruments (Items_Code ,I_Type) values ('8499141615449', 'Performing Surgical Incision');
-insert into Instruments (Items_Code ,I_Type) values ('5814401496275', 'Pluse And Blood Oxgen Pressure Measurement');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('9891957382168', 'Parenteral dosage form administration', '102');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('9236706976075', 'Urine Collection', '4');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('9160945837954', 'Venous Dosage From Administration', '9');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('4001883254479', 'Nebulized-Durg Administration', '7');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('5648875210671', 'Body Mass Measurement And Calculation Of Body Water, Lipid, Protein, And Carbohydrate', '1');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('4168514353850', 'Measurement Of Blood Pressure', '3');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('6965105140966', 'Pregnancy Testing', '12');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('9158394740278', 'Blood Glucose Testing', '2');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('8499141615449', 'Performing Surgical Incision', '10');
+insert into Instruments (Items_Code ,I_Type, Stock_Level) values ('5814401496275', 'Pluse And Blood Oxgen Pressure Measurement', '2');
